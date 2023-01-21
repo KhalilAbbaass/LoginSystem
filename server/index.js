8e55cc7410702002dfc9469a5fc20a9ebf6210a4
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-01-21 10:53:49 +0200

    Server side installations and database connection

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+
+dotenv.config();
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Database connection successfull"))
+.catch((err) => {
+    console.log(err);
+});
+
+app.use(cors());
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+    console.log("Backend server is running")
+})
