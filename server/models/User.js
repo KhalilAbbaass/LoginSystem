b5154b0bc1e80c39976b9a2db5aebb41ec60d854
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-01-26 13:39:58 +0200

    image type changed to string

--- server/models/User.js
+++ server/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
             type: String,
         },
         image:{
-            type: Buffer
+            type: String
         },
         password:{
             type: String,
