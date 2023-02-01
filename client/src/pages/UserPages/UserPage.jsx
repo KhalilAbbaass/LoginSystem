40fb4763cc426bd5f554bb8a77bc9b5ca24826cb
KhalilAbbaass <khaleel.abbaass@gmail.com>
2023-02-01 13:31:19 +0200

    userpage design

--- client/src/pages/UserPages/UserPage.jsx
+++ client/src/pages/UserPages/UserPage.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import "./UserPage.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 
 
 const UserPage = () => {
+
+    const allQuotes = ['1','2','3','4','5','6','7','8']
+
+
   return (
     <div>
-        <h1>Welcome to user page</h1>
+        <h4 className='h4Style'>Welcome , you are logged in as a user</h4>
+        <Row > 
+
+        {allQuotes.map((item, i) => (
+            <Col className='colStyle' xs={6} md = {4} lg = {3} key={i}>
+            <Card
+                bg=''
+                
+                text=''
+                style={{ width: '18rem' }}
+                className=" cardStyle mb-2"
+                >
+                
+                <Card.Body>
+                    <h3> Quote {i} </h3>
+                    <p>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </p>
+                </Card.Body>
+            </Card>
+            </Col>
+        ))}
+       </Row>
+
     </div>
   )
 }
